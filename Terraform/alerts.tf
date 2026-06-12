60d136486170411b728f64af7de9c83533cee89f
ivan-saji <ivansajimeleth@gmail.com>
2026-06-12 11:11:29 +0530

    Setup Memory Alerts

--- Terraform/alerts.tf
+++ Terraform/alerts.tf
@@ -5,8 +5,8 @@ resource "azurerm_monitor_action_group" "email_alerts" {
   short_name          = "emailAlerts"
 
   email_receiver {
-    name          = "VMSS Alert"
-    email_address = local.alert_mail_to
+    name                    = "VMSS Alert"
+    email_address           = local.alert_mail_to
     use_common_alert_schema = true
   }
 }
@@ -19,14 +19,14 @@ resource "azurerm_monitor_metric_alert" "cpu-alert-95" {
   scopes              = [azurerm_linux_virtual_machine_scale_set.vmss01.id]
   description         = "Action will be triggered when CPU usage is greater than 95%."
 
-  frequency = "PT1M"
-  severity  = 0
+  frequency   = "PT1M"
+  severity    = 0
   window_size = "PT5M"
 
   criteria {
     metric_namespace = "Microsoft.Compute/virtualMachineScaleSets"
     metric_name      = "Percentage CPU"
-    aggregation      = "Maximum"
+    aggregation      = "Average"
     operator         = "GreaterThan"
     threshold        = local.alert_thresholds.cpu_critical
   }
@@ -41,14 +41,14 @@ resource "azurerm_monitor_metric_alert" "cpu-alert-80" {
   scopes              = [azurerm_linux_virtual_machine_scale_set.vmss01.id]
   description         = "Action will be triggered when CPU usage is greater than 80%."
 
-  frequency = "PT1M"
-  severity  = 2
+  frequency   = "PT1M"
+  severity    = 2
   window_size = "PT5M"
 
   criteria {
     metric_namespace = "Microsoft.Compute/virtualMachineScaleSets"
     metric_name      = "Percentage CPU"
-    aggregation      = "Maximum"
+    aggregation      = "Average"
     operator         = "GreaterThan"
     threshold        = local.alert_thresholds.cpu_warning
   }
@@ -56,4 +56,84 @@ resource "azurerm_monitor_metric_alert" "cpu-alert-80" {
   action {
     action_group_id = azurerm_monitor_action_group.email_alerts.id
   }
+}
+
+#Memory Alert Rules
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "memory_warning-80" {
+
+  name                = "vmss-memory-warning-80"
+  location            = azurerm_resource_group.rg03-monitoring.location
+  resource_group_name = azurerm_resource_group.rg03-monitoring.name
+  description         = "Action will be triggered when Memory usage is greater than 80%."
+
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+
+  scopes = [
+    azurerm_log_analytics_workspace.law01.id
+  ]
+
+  severity = 2
+
+  criteria {
+
+    query = <<QUERY
+Perf
+| where CounterName == "% Committed Bytes In Use"
+| summarize AvgMemory = avg(CounterValue) by bin(TimeGenerated, 5m)
+QUERY
+
+    operator  = "GreaterThan"
+    threshold = local.alert_thresholds.memory_warning
+
+    time_aggregation_method = "Average"
+
+  }
+
+  action {
+    action_groups = [
+      azurerm_monitor_action_group.email_alerts.id
+    ]
+  }
+
+}
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "memory_warning-95" {
+
+  name                = "vmss-memory-warning-95"
+  location            = azurerm_resource_group.rg03-monitoring.location
+  resource_group_name = azurerm_resource_group.rg03-monitoring.name
+  description         = "Action will be triggered when Memory usage is greater than 95%."
+
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+
+  scopes = [
+    azurerm_log_analytics_workspace.law01.id
+  ]
+
+  severity = 0
+
+  criteria {
+
+    query = <<QUERY
+Perf
+| where CounterName == "% Committed Bytes In Use"
+| summarize AvgMemory = avg(CounterValue) by bin(TimeGenerated, 5m)
+QUERY
+
+    operator  = "GreaterThan"
+    threshold = local.alert_thresholds.memory_critical
+
+    time_aggregation_method = "Average"
+
+  }
+
+  action {
+    action_groups = [
+      azurerm_monitor_action_group.email_alerts.id
+    ]
+  }
+
 }
